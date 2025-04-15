b0e1661702dd78c8dd7ef473ddec53394b4009d1
sumit kumar <sumitrwk90@gmail.com>
2025-04-15 16:27:50 +0530

    solver syntex error

--- example.py
+++ example.py
@@ -1,15 +1,14 @@
 import math
 
+
 def greet(name):
     print(f"Hello {name}")
 
 
-
-
-
-
-    def add(a, b):
-        return a+b
+def add(a, b):
+    return a + b
     
-    greet("Anil")
-    print(add(3,5))
+
+greet("Anil")
+
+print(add(3,5))
